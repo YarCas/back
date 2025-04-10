c2ce9a9869d693c58da62b4c6d2cbe9f7417fb2c
YarCas <izmaylov0601@list.ru>
2025-04-10 17:19:51 +0300

    Обновление конфигурации для деплоя

--- server.js
+++ server.js
@@ -38,8 +38,14 @@ const sequelize = new Sequelize({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
-});
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Если у вас самоподписанный сертификат
+        }
+}}
+);
 
 // Проверка подключения к базе данных
 sequelize.authenticate()
